ace44c931bde343816b051b71fe0d267de47c4dd
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-08-28 13:17:56 +0500

    Implemented ApplicationRepository

---
+++ Qualification.Data/IRepositories/IApplicationRepository.cs
@@ -0,0 +1,12 @@
+﻿using Qualification.Domain.Entities;
+
+namespace Qualification.Data.IRepositories;
+
+public interface IApplicationRepository
+{
+    ValueTask<Application> InsertApplicationAsync(Application application);
+    IQueryable<Application> SelectAllApplications();
+    ValueTask<Application> SelectApplicationByIdAsync(long applicationId);
+    ValueTask<Application> UpdateApplicationAsync(Application application);
+    ValueTask<Application> DeleteApplicationAsync(Application application);
+}
