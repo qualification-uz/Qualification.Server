ace7a3b4ef4ab0f135bb555c074ae9addccc6d05
iCloudBMX <iCloudBMX0525@gmail.com>
2022-09-20 16:02:33 +0500

    Quiz respositories contracts

---
+++ Qualification.Data/IRepositories/IQuizQuestionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Qualification.Domain.Entities.Quizes;
+
+namespace Qualification.Data.IRepositories;
+
+public interface IQuizQuestionRepository
+{
+    IQueryable<QuizQuestion> SelectAllQuizes();
+    ValueTask<QuizQuestion> SelectQuestionByIdAsync(long questionId);
+    ValueTask<QuizQuestion> InsertQuizQuestionAsync(QuizQuestion quizQuestion);
+    ValueTask<QuizQuestion> UpdateQuizQuestionAsync(QuizQuestion quizQuestion);
+    ValueTask<QuizQuestion> DeleteQuizQuestionAsync(QuizQuestion quizQuestion);
+}
---
+++ Qualification.Data/IRepositories/ISubmissionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Qualification.Domain.Entities.Quizes;
+
+namespace Qualification.Data.IRepositories;
+
+public interface ISubmissionRepository
+{
+    IQueryable<Submission> SelectAllSubmissions();
+    ValueTask<Submission> SelectSubmissionByIdAsync();
+    ValueTask<Submission> InsertSubmissionAsync(Submission submission);
+    ValueTask<Submission> UpdateSubmissionAsync(Submission submission);
+    ValueTask<Submission> DeleteSubmissionAsync(Submission submission);
+}
