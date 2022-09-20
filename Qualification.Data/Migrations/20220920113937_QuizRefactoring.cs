e426509e5a12d450bed0c35f8bfda08f6a293c29
iCloudBMX <iCloudBMX0525@gmail.com>
2022-09-20 16:40:41 +0500

    Quiz refactoring

---
+++ Qualification.Data/Migrations/20220920113937_QuizRefactoring.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Qualification.Data.Migrations
+{
+    public partial class QuizRefactoring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionOptionAsset");
+
+            migrationBuilder.DropTable(
+                name: "QuizQuestionAsset");
+
+            migrationBuilder.DropTable(
+                name: "QuestionOption");
+
+            migrationBuilder.DropTable(
+                name: "QuizQuestions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuizQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    QuizId = table.Column<long>(type: "bigint", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizQuestions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuizQuestions_Quizes_QuizId",
+                        column: x => x.QuizId,
+                        principalTable: "Quizes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionOption",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    QuizQuestionId = table.Column<long>(type: "bigint", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    IsCorrect = table.Column<bool>(type: "boolean", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionOption", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionOption_QuizQuestions_QuizQuestionId",
+                        column: x => x.QuizQuestionId,
+                        principalTable: "QuizQuestions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuizQuestionAsset",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    QuizQuestionId = table.Column<long>(type: "bigint", nullable: false),
+                    AssetId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuizQuestionAsset", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuizQuestionAsset_QuizQuestions_QuizQuestionId",
+                        column: x => x.QuizQuestionId,
+                        principalTable: "QuizQuestions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionOptionAsset",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    QuestionOptionId = table.Column<long>(type: "bigint", nullable: false),
+                    AssetId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionOptionAsset", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionOptionAsset_QuestionOption_QuestionOptionId",
+                        column: x => x.QuestionOptionId,
+                        principalTable: "QuestionOption",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionOption_QuizQuestionId",
+                table: "QuestionOption",
+                column: "QuizQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionOptionAsset_QuestionOptionId",
+                table: "QuestionOptionAsset",
+                column: "QuestionOptionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizQuestionAsset_QuizQuestionId",
+                table: "QuizQuestionAsset",
+                column: "QuizQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuizQuestions_QuizId",
+                table: "QuizQuestions",
+                column: "QuizId");
+        }
+    }
+}
--- Qualification.Domain/Entities/Quizes/QuestionOption.cs
+++ Qualification.Domain/Entities/Quizes/QuestionOption.cs
@@ -4,11 +4,7 @@ namespace Qualification.Domain.Entities.Quizes;
 
 public class QuestionOption : Auditable
 {
-    public string Content { get; set; }
-    public bool IsCorrect { get; set; }
-
-    public long QuizQuestionId { get; }
-    public QuizQuestion Question { get; }
-
-    public ICollection<QuestionOptionAsset> Assets { get; set; }
+    public long QuizQuestionId { get; set; }
+    public long QuizOptionId { get; set; }
+    public short ShufflePosition { get; set; }
 }
